ef8957d5a8ca77a9379bd3247ac9c20ef8d65de2
tagivis <tagivis@hotmail.com>
2019-06-11 16:18:25 -0500

    first commit spacejam

---
+++ src/todayPic/todayPicComponent.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
+const useStyles = makeStyles({
+    card: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 140,
+    },
+    });
+
+
+
+const TodayPicComponent = ({
+    key,
+    id,
+    date,
+    explanation,
+    hdurl,
+    media_type,
+    title,
+    url 
+}) => {
+
+    
+    const classes = useStyles();
+    return (
+
+    <Card className={classes.card}>
+        <CardActionArea>
+        <CardMedia
+            className={classes.media}
+            image= {url}
+            title={title}
+        />
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+            {title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+            {explanation}
+            </Typography>
+        </CardContent>
+        </CardActionArea>
+        <CardActions>
+        <Button size="small" color="primary">
+            Go to site
+        </Button>        
+        </CardActions>
+    </Card>
+    );
+
+
+
+}
+export default TodayPicComponent
---
+++ src/todayPic/todayPicContainer.jsx
@@ -0,0 +1,65 @@
+import  React, {Component} from "react";
+import  axios  from "axios";
+import TodayPicComponent from "./todayPicComponent";
+
+
+const urlApod='https://api.nasa.gov/planetary/apod?api_key=gaAbODbeimgPyb3qKIhBGYTCIfaTwY3J5sVg2Che'
+
+class TodayPicContainer extends Component{
+
+    // state={
+    //     key:'',
+    //     id:'',
+    //     date:'',
+    //     explanation:'',
+    //     hdurl:'',
+    //     media_type:'',
+    //     title:'',
+    //     url:'' 
+    //  }
+
+    state={
+        dayPicture: []
+    }
+    
+
+    componentDidMount()
+    {
+        this.getApod();
+    }
+
+   async getApod()
+    {
+       try
+       {
+            const res = await axios.get(urlApod);
+            this.setState({ dayPicture: res.data })     
+        }
+        catch(error)
+        {
+            console.log(error)
+        }
+    }
+
+
+
+    render()
+    {
+        console.log("Pictures", this.state.dayPicture);
+        return(        
+            <>
+                <TodayPicComponent
+                    id={this.state.dayPicture.date}
+                    date={this.state.dayPicture.date}
+                    explanation={this.state.dayPicture.explanation}
+                    hdurl= {this.state.dayPicture.hdurl}
+                    media_type ={this.state.dayPicture.media_type}
+                    title= {this.state.dayPicture.title}
+                    url= {this.state.dayPicture.url}
+                    />
+            </>
+        
+        );
+    }
+}
+export default TodayPicContainer;
